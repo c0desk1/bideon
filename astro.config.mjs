c617941934508d869d0a64f50e232029958f031a
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-28 02:06:01 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,16 @@ import { defineConfig } from 'astro/config';
 // Import resmi dari Astro
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [solid()],
-
-  vite: {
+vite: {
     plugins: [tailwindcss()],
   },
+  site: 'https://example.com',
+  integrations: [solid()]
+
   
-  // Kita tidak perlu lagi konfigurasi vite manual untuk Tailwind
-  // Jadi, bagian 'vite' bisa dihapus jika hanya untuk itu.
 });
