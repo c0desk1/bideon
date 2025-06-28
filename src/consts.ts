2eac04eefc45a032f1cb75670d8f847b48e291be
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-29 00:43:57 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,9 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Bima Akbar';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to Bima Akbar a.k.a Bideon
+
+Sometimes I play games during live, so don't forget to stay tuned!
+
+Disclaimer!
+Many tracks are originally composed and produced by me using FL Studio, ensuring full creative control. These tracks are also royalty-free and copyright-free, making them available for use in your projects with proper credit (if required).';
