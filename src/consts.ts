2a6a9052ef6969d3fa602515f3478de2e0288ef9
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-29 20:28:31 +0700

    Delete src directory

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Bima Akbar';
-export const SITE_DESCRIPTION = 'Welcome to Bima Akbar a.k.a Bideon. Thank you for your support!';
