da863da0d9002e2258dc9a89891e09e750a41645
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-06-30 12:53:26 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+adapter: vercel(),
   integrations: [
     react({
       ssr: true,
