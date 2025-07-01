de93fbe25088e629e62746014365b18e56502768
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-01 07:39:09 +0700

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 // src/content/config.ts
-import { defineCollection, z } from 'astro:content';
-
 export const POSTS_PER_PAGE = 3;
+
+import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
   type: 'content',
