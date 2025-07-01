ec521c897f9156572a93468c673ea881bfbc317a
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-02 02:07:10 +0700

    Delete src directory

--- src/content/blog/dsfsdf.md
+++
@@ -1,9 +0,0 @@
----
-title: "dsfsdf"
-description: "sdfsdfs"
-pubDate: 2025-07-01T16:07:32.447Z
-author: "Bima Akbar" # Atau ambil dari user yang login nanti
-draft: true # Buat sebagai draf dulu
----
-
-fsdfsdf
--- src/services/readingTime.ts
+++
@@ -1,8 +0,0 @@
-// src/utils/readingTime.ts
-export function calculateReadingTime(content: string): string {
-    const wordsPerMinute = 200;
-    const numberOfWords = content.trim().split(/\s+/).length;
-    const minutes = Math.ceil(numberOfWords / wordsPerMinute);
-    return `${minutes} menit baca`;
-  }
-  
