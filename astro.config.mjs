1230fc6016cec6c580a3c7036efc13366543587b
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-01 08:26:49 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-nocheck
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import netlify from '@astrojs/netlify';
+
+// Ditambahkan untuk alias path
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Ditambahkan untuk alias path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
@@ -25,7 +29,11 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    // Ditambahkan untuk alias path
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
   },
-
-  
 });
