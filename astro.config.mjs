d840de9073d91f6a4246ee94e7c8934ab859d586
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-04 03:32:18 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,10 @@
-// @ts-nocheck
+// @ts-check
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import netlify from '@astrojs/netlify';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bimaakbar.my.id',
-  integrations: [react(), sitemap()],
-  output: 'server',
-  adapter: netlify({
-    imageCDN: false,
-  }),
-
-  image: {
-    service: {
-      // @ts-ignore
-      entry: 'astro/assets/services/compile'
-    }
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        '@': new URL('./src', import.meta.url).pathname
+    vite: {
+        plugins: [tailwindcss()],
       },
-    },
-  },
 });
