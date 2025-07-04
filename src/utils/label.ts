148bdefcb7a9e74a407c387a4b3494b50a9ca17a
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-04 18:46:21 +0700

    Delete src directory

--- src/utils/label.ts
+++
@@ -1,9 +0,0 @@
-// utils/label.ts
-export function labelToSlug(label: string): string {
-    return label.toLowerCase().replace(/\s+/g, '-');
-  }
-  
-  export function slugToLabel(slug: string): string {
-    return slug.replace(/-/g, ' ');
-  }
-  
