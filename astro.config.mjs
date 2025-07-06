2a7b95832c07865f096ec194d15fa2ffaba901dc
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-06 10:04:01 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bimaakbar.my.id',
+  integrations: [sitemap()],
   vite: {
       plugins: [tailwindcss()],
     },
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+ }),
 });
