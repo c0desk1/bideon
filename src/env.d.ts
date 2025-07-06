cb0fde81fc3ec51028841ea8cfaca230176db78c
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-07 02:05:33 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
 // src/env.d.ts
 
 /// <reference types="astro/client" />
-
+type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
-    interface Locals {
+  interface Locals extends Runtime {
       blogName: string;
       blogData: {
         name: string;
