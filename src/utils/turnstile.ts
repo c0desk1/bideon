831b780f7b9911e7f26063b2ce82de0762da0776
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-06 20:50:45 +0700

    Update turnstile.ts

--- src/utils/turnstile.ts
+++ src/utils/turnstile.ts
@@ -1,5 +1,5 @@
 export async function verifyTurnstile(token: string): Promise<boolean> {
-    const secret = import.meta.env.TURNSTILE_SECRET_TOKEN;
+    const secret = import.meta.env.VITE_TURNSTILE_SECRET_TOKEN;
     if (!secret || !token) return false;
   
     const res = await fetch("https://challenges.cloudflare.com/turnstile/v0/siteverify", {
