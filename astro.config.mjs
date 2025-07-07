f828ba7908f8007d78448f9d56966671defe914a
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-07 11:18:29 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,32 @@
 // @ts-nocheck
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bimaakbar.my.id',
-	output: 'server',
-	adapter: cloudflare({
-		imageService: 'cloudflare',
-
-		platformProxy: {
-			enabled: true,
-			configPath: 'wrangler.jsonc',
-			persist: {
-				path: './.cache/wrangler/v3',
-			},
-		},
-
-		sessionKVBindingName: 'BIMA_KV_SPACE',
-
-		bindings: {
-			BIMA_KV_SPACE: {
-				type: 'kv',
-				namespace_id: process.env.CLOUDFLARE_KV_NAMESPACE_ID,
-			},
-		},
-		
-		integrations: [sitemap()],
-		
-		vite: {
-			plugins: [tailwindcss()],
-		},
-	}),
+  site: 'https://bimaakbar.my.id',
+  integrations: [sitemap()],
+  vite: {
+      plugins: [tailwindcss()],
+    },
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+    platformProxy: {
+      enabled: true,
+      configPath: 'wrangler.jsonc',
+      persist: {
+        path: './.cache/wrangler/v3'
+      },
+    },
+    bindings: {
+      BIMA_KV_SPACE: {
+        type: 'kv',
+        namespace_id: process.env.CLOUDFLARE_KV_NAMESPACE_ID,
+      },
+    },
+    sessionKVBindingName: 'BIMA_KV_SPACE',
+ }),
 });
