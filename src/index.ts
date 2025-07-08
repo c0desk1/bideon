c0bd50b17f46600ebbfd252acd8f98dd1d4f6b06
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-08 21:10:25 +0700

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+export interface Env {
+    BIMA_KV_SPACE: KVNamespace;
+  }
+  
+  export default {
+    async fetch(req: Request, env: Env): Promise<Response> {
+      const url = new URL(req.url);
+
+      if (url.pathname === "/api/stat/visitors") {
+        const today = new Date().toISOString().split("T")[0];
+        const key = `visits:${today}`;
+        const count = await env.BIMA_KV_SPACE.get(key);
+        return new Response(JSON.stringify({
+          date: today,
+          count: parseInt(count ?? "0"),
+        }), {
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+
+      return new Response("OK");
+    }
+  }
+  
