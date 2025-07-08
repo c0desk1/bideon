51f6b2a23667c2b86a088ddca04b0a35d0e7c9a0
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-08 22:19:54 +0700

    Delete src/pages/api/status.ts

--- src/pages/api/status.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-    const res = await fetch("https://bima-akbar-dev.bimasaktiakbarr.workers.dev");
-    const data = await res.json();
-    return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
-  
