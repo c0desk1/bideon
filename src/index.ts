00943d9ce734990716405313c1f493c3119b7575
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-08 21:18:42 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export interface Env {
     async fetch(req: Request, env: Env): Promise<Response> {
       const url = new URL(req.url);
 
-      if (url.pathname === "/api/stat/visitors") {
+      if (url.pathname === "/api/visitor") {
         const today = new Date().toISOString().split("T")[0];
         const key = `visits:${today}`;
         const count = await env.BIMA_KV_SPACE.get(key);
