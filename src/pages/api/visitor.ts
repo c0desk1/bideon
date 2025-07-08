4425828c3763a0df2b09b797b4d4e303ffeedfd1
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-08 21:32:04 +0700

    Update visitor.ts

--- src/pages/api/visitor.ts
+++ src/pages/api/visitor.ts
@@ -1,5 +1,5 @@
 export async function GET() {
-    const res = await fetch("https://bima-akbar-dev.bimasaktiakbarr.workers.dev/api/visitor");
+    const res = await fetch("https://bimaakbar.my.id/api/visitor");
     const data = await res.json();
     return new Response(JSON.stringify(data), {
       headers: { 'Content-Type': 'application/json' },
