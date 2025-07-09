f4177cea34fc55769afdfb40b1d4eab3e2c321d6
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-09 22:18:05 +0700

    Add files via upload

---
+++ src/utils/buttondown.ts
@@ -0,0 +1,9 @@
+export async function subscribeToButtondown(email: string): Promise<boolean> {
+  const res = await fetch("https://buttondown.email/api/emails/embed-subscribe/bimaakbar", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({ email, embed: "1" }),
+  });
+
+  return res.ok;
+}
