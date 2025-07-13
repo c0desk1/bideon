58767961de5f21eb8e3495fd062ee87ab2953953
Bima Akbar <81931118+c0desk1@users.noreply.github.com>
2025-07-13 11:13:01 +0700

    Delete src directory

--- src/utils/handlers/authLogin.ts
+++
@@ -1,8 +0,0 @@
-// utils/handlers/authLogin.ts
-import { generateAuthURL } from "../google-auth";
-import type { Env } from "../../lib/types";
-
-export async function handleAuthLogin(env: Env): Promise<Response> {
-  const authUrl = generateAuthURL(env);
-  return Response.redirect(authUrl, 302);
-}
